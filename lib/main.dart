209694477798432817683eea65a151a59d30f9dd
Joseph Haydn Arcila Amaya <joseph@Miyus-Air.lan>
2024-05-29 11:51:11 +0900

    Implement authentication state persistence and initial navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:wil_doc/screens/home/scan_document_screen.dart';
+import 'package:wil_doc/screens/auth/login_screen.dart';
 import 'package:wil_doc/utils/theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: FirebaseOptions(
+    options: const FirebaseOptions(
       apiKey: "AIzaSyBDo5W2woIYsKuycY5mkpVDDmen5R8TYlI",
       authDomain: "wildocjapan.firebaseapp.com",
       projectId: "wildocjapan",
@@ -27,7 +29,29 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'WiLDoc',
       theme: lightHighContrastTheme,
-      home: const ScanDocumentScreen(),
+      home: const AuthWrapper(),
+    );
+  }
+}
+
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        } else if (snapshot.hasData) {
+          return const ScanDocumentScreen();
+        } else {
+          return const LoginScreen();
+        }
+      },
     );
   }
 }
