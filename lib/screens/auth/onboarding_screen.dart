48e1860843ed6a86e51dd0f367ead6c07bea65cc
Joseph Haydn Arcila Amaya <joseph@Miyus-Air.lan>
2024-06-01 21:58:14 +0900

    refactor: move constants to a separate file for better code organization
    - Created  in
    - Moved nationality, visa status, prefecture, and proficiency lists to
    - Updated imports in  to use the new constants file

--- lib/screens/auth/onboarding_screen.dart
+++ lib/screens/auth/onboarding_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:wil_doc/routes/app_routes.dart';
 import 'package:wil_doc/widgets/custom_text_field.dart';
 import 'package:wil_doc/widgets/custom_dropdown_menu.dart';
+import 'package:wil_doc/utils/constants.dart'; // Add this import
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
@@ -324,23 +325,3 @@ class FinishButton extends StatelessWidget {
     );
   }
 }
-
-// List of nationalities
-const List<String> nationalityList = [
-  'American', 'Colombian', 'Indian', 'Nepali'
-];
-
-// List of visa statuses
-const List<String> visaStatusList = [
-  'Student', 'Work', 'Dependent', 'Permanent Resident'
-];
-
-// List of prefectures
-const List<String> prefectureList = [
-  'Hokkaido', 'Aomori', 'Iwate', 'Miyagi', 'Akita', 'Yamagata', 'Fukushima', 'Ibaraki', 'Tochigi', 'Gunma', 'Saitama', 'Chiba', 'Tokyo', 'Kanagawa', 'Niigata', 'Toyama', 'Ishikawa', 'Fukui', 'Yamanashi', 'Nagano', 'Gifu', 'Shizuoka', 'Aichi', 'Mie', 'Shiga', 'Kyoto', 'Osaka', 'Hyogo', 'Nara', 'Wakayama', 'Tottori', 'Shimane', 'Okayama', 'Hiroshima', 'Yamaguchi', 'Tokushima', 'Kagawa', 'Ehime', 'Kochi', 'Fukuoka', 'Saga', 'Nagasaki', 'Kumamoto', 'Oita', 'Miyazaki', 'Kagoshima', 'Okinawa'
-];
-
-// List of Japanese proficiency levels
-const List<String> proficiencyList = [
-  'Beginner', 'Intermediate', 'Advanced', 'Native'
-];
---
+++ lib/utils/constants.dart
@@ -0,0 +1,21 @@
+// lib/utils/constants.dart
+
+// List of nationalities
+const List<String> nationalityList = [
+  'American', 'Colombian', 'Indian', 'Nepali'
+];
+
+// List of visa statuses
+const List<String> visaStatusList = [
+  'Student', 'Work', 'Dependent', 'Permanent Resident'
+];
+
+// List of prefectures
+const List<String> prefectureList = [
+  'Hokkaido', 'Aomori', 'Iwate', 'Miyagi', 'Akita', 'Yamagata', 'Fukushima', 'Ibaraki', 'Tochigi', 'Gunma', 'Saitama', 'Chiba', 'Tokyo', 'Kanagawa', 'Niigata', 'Toyama', 'Ishikawa', 'Fukui', 'Yamanashi', 'Nagano', 'Gifu', 'Shizuoka', 'Aichi', 'Mie', 'Shiga', 'Kyoto', 'Osaka', 'Hyogo', 'Nara', 'Wakayama', 'Tottori', 'Shimane', 'Okayama', 'Hiroshima', 'Yamaguchi', 'Tokushima', 'Kagawa', 'Ehime', 'Kochi', 'Fukuoka', 'Saga', 'Nagasaki', 'Kumamoto', 'Oita', 'Miyazaki', 'Kagoshima', 'Okinawa'
+];
+
+// List of Japanese proficiency levels
+const List<String> proficiencyList = [
+  'Beginner', 'Intermediate', 'Advanced', 'Native'
+];
