03af84c2f1d7f314519f50abd91894a21867cd6c
Joseph Haydn Arcila Amaya <joseph@Miyus-Air.lan>
2024-06-18 16:42:27 +0900

    Fix: Resolve private type usage in public API for DocumentSummaryScreen
    - Changed _DocumentSummaryScreenState to DocumentSummaryScreenState to adhere to public API conventions.
    - Updated TabBar to use filled icons for active tabs in DocumentSummaryScreen.

--- lib/screens/document/document_summary_screen.dart
+++ lib/screens/document/document_summary_screen.dart
@@ -1,31 +1,60 @@
 import 'package:flutter/material.dart';
 
-class DocumentSummaryScreen extends StatelessWidget {
+class DocumentSummaryScreen extends StatefulWidget {
   const DocumentSummaryScreen({super.key});
 
   @override
+  DocumentSummaryScreenState createState() => DocumentSummaryScreenState();
+}
+
+class DocumentSummaryScreenState extends State<DocumentSummaryScreen> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          centerTitle: false,
-          title: const Text('Document Scan'),
-          bottom: const TabBar(
-            tabs: [
-              Tab(text: 'Summary', icon: Icon(Icons.summarize_outlined)),
-              Tab(text: 'Translation', icon: Icon(Icons.translate_outlined)),
-              Tab(text: 'Guide', icon: Icon(Icons.support_outlined)),
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: const Text('Document Scan'),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(
+              text: 'Summary',
+              icon: Icon(_tabController.index == 0 ? Icons.summarize : Icons.summarize_outlined),
+            ),
+            Tab(
+              text: 'Translation',
+              icon: Icon(_tabController.index == 1 ? Icons.translate : Icons.translate_outlined),
+            ),
+            Tab(
+              text: 'Guide',
+              icon: Icon(_tabController.index == 2 ? Icons.support_rounded : Icons.support_outlined),
+            ),
+          ],
+          onTap: (index) {
+            setState(() {});
+          },
         ),
-        body: const Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircularProgressIndicator(),
-            ],
-          ),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircularProgressIndicator(),
+          ],
         ),
       ),
     );
